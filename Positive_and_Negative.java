bf96e306773c383737129d5199823c337b153c7e
JerrieEdwin <67513630+JerrieEdwin@users.noreply.github.com>
2021-09-05 00:14:45 +0530

    Add files via upload

---
+++ Positive_and_Negative.java
@@ -0,0 +1,24 @@
+package week1.day1.assignments;
+
+public class Positive_and_Negative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int number = -1;
+
+		if (number < 0) {
+			System.out.println("The Number is negative");
+		}
+
+		else if (number == 0) {
+			System.out.println("The Number is Zero");
+		}
+
+		else {
+			System.out.println("The Number is positive");
+		}
+
+	}
+
+}
